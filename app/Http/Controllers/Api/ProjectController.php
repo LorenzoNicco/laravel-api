256b725ab4ca97f48add1be6f607a2add1a36529
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-30 15:51:40 +0200

    Add stampa in pagina tecnologie

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::with('type')->paginate(10);
+        $projects = Project::with('type', 'technologies')->paginate(10);
 
         $response = [
             'success' => true,
